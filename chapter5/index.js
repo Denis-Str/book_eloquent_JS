5dd80dcfeb1fe3608086d2b5b307ddda75e84613
Denis <web_life@inbox.ru>
2019-09-05 13:11:05 +0300

    new func

--- chapter5/index.js
+++ chapter5/index.js
@@ -17,3 +17,17 @@ const loop = (value, condition, iter, func) => {
 };
 
 loop(3, n => n > 0, n => n - 1, console.log);
+
+const every = (array, test) => {
+   for (let value of array) {
+       if (!test(value)) return false;
+   }
+   return true
+};
+
+console.log(every([1, 3, 5], n => n < 10));
+// // → true
+console.log(every([2, 4, 16], n => n < 10));
+// // → false
+console.log(every([], n => n < 10));
+// // → true
